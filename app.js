347da7d6339e4167b368adcc840978f134838004
Nat Sothanaphan <natsothanaphan@gmail.com>
2023-10-15 19:00:01 +0700

    feat: env vars

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-import express from "express";
+import express from 'express';
+import 'dotenv/config'
 
 const app = express();
 app.use(express.json());
 app.get('/ping', (req, res) => {
     res.json({timestamp: new Date().toISOString()});
 });
-app.listen(3000);
+app.listen(process.env.PORT);
