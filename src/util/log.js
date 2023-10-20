ad2e18fb2fd1c4a71fb0eb4ced6c1882d1d8c9c0
Nat Sothanaphan <nat.sothanaphan@scbabacus.com>
2023-10-20 23:47:48 +0700

    fix: log indent

--- src/util/log.js
+++ src/util/log.js
@@ -1,10 +1,12 @@
+const indent = process.env.LOG_INDENT ? parseInt(process.env.LOG_INDENT) : null;
+
 const log = (message, extra = {}) => {
     const entry = {
         timestamp: new Date().toISOString(),
         message,
         ...extra,
     };
-    console.log(JSON.stringify(entry, null, 2));
+    console.log(JSON.stringify(entry, null, indent));
 };
 
 export default {log};
