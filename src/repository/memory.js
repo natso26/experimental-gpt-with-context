900e08b9f4d094cec25b482be36cc5f3a014bdda
Nat Sothanaphan <nat.sothanaphan@scbabacus.com>
2023-10-25 15:05:31 +0700

    feat: progress

--- src/repository/memory.js
+++ src/repository/memory.js
@@ -29,4 +29,45 @@ const search = wrapper.logCorrelationId('repository.memory.search', async (corre
         .slice(0, numResults);
 });
 
+const consolidate = wrapper.logCorrelationId('repository.memory.consolidate', async (correlationId, chatId, consolidationFn) => {
+    const eltsRef = await coll.doc(chatId).collection('elts');
+    const snapshot = await eltsRef.orderBy('index', 'desc').limit(1).get();
+    const latestIndex = snapshot.empty ? -1 : snapshot.docs[0].data().index;
+    if (latestIndex < 7) {
+        return;
+    }
+    const consolidationsRef = await coll.doc(chatId).collection('consolidations');
+    let consolidationLevel = 0;
+    while (true) {
+        let targetConsolidationIndex = Math.floor((latestIndex - 7) / 4);
+        for (let i = 0; i < consolidationLevel; i++) {
+            targetConsolidationIndex = Math.floor((targetConsolidationIndex + 1) / 4) - 1;
+        }
+        if (targetConsolidationIndex < 0) {
+            break;
+        }
+        await db.runTransaction(async (txn) => {
+            const snapshot = await consolidationsRef.orderBy('index', 'desc').limit(1).get();
+
+        });
+        const latestConsolidationIndex = snapshot.empty ? -1 : snapshot.docs[0].data().index;
+        if (targetConsolidationIndex <= latestConsolidationIndex) {
+            return;
+        }
+        for (let i = latestConsolidationIndex + 1; i <= targetConsolidationIndex; i++) {
+            const consolidationRef = consolidationsRef.doc();
+            await db.runTransaction(async (txn) => {
+                const snapshot = await txn.get(eltsRef.orderBy('index', 'desc')
+                    .offset(i * 4).limit(4));
+                const elts = snapshot.docs.map(doc => doc.data().elt);
+                await txn.set(consolidationRef, {
+                    index: i,
+                    elts,
+                });
+            });
+        }
+        consolidationLevel++;
+    }
+});
+
 export default {add, search};
